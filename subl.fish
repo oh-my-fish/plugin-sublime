fd97e9b128129cdc68ddfcbb9cb4788b3e0a8ad3
Justin Hileman <justin@justinhileman.info>
2015-08-30 08:27:24 -0700

    Automatically load project, if applicable.
    If one (and only one) directory is given, and it includes a single `*.sublime-project` file, load that project rather than just opening the directory.

--- subl.fish
+++ subl.fish
@@ -5,6 +5,30 @@
 #
 
 function subl -d "Open Sublime Text"
+  set -l opts
+  set -l files
+  set -l projects
+
+  # we'll just ignore anything that looks like an option string...
+  for file in $argv
+    switch $file
+      case '-*'
+        set opts $opts $file
+
+      case '.*' '*'
+        set files $files $file
+    end
+  end
+
+  # if there's one (and only one) *.sublime-project file in the folder listed, open that instead.
+  if [ (count $files) -eq 1 ]
+    set projects (find $files[1] -name "*.sublime-project" -maxdepth 1 2> /dev/null)
+  end
+
+  if [ (count $projects) -eq 1 ]
+    set argv $opts $projects[1]
+  end
+
   if begin; which subl > /dev/null; and test -x (which subl); end
     command subl $argv
   else if test -d "/Applications/Sublime Text.app"
